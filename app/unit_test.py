2afc8de5ea256d38d2dd07115c07c4248141cc7b
Chaksawat <88503339+Nonene2546@users.noreply.github.com>
2024-09-23 15:25:36 +0700

    asdf

--- app/unit_test.py
+++ app/unit_test.py
@@ -2,11 +2,19 @@ import unittest
 from werkzeug import exceptions
 
 import app
+from app import app as flask_instance
 
 class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app_context = flask_instance.app_context()
+        self.app_context.push()
+
+    def tearDown(self):
+        self.app_context.pop()
+    
     def test_happy_plus(self):
-        res = app.plus(1, 4).response.pop()
-        self.assertEqual(res, b'3\n')
+        res = app.plus(1, 4)
+        self.assertEqual(res.get_json(), 5)
     
     def test_first_num_invalid(self):
         with self.assertRaises(exceptions.BadRequest):
@@ -21,5 +29,4 @@ class AppTestCase(unittest.TestCase):
             app.plus("2+9/5", "2+9/5")
 
 if __name__ == "__main__":
-    with app.app.app_context():
-        unittest.main()
+    unittest.main()
